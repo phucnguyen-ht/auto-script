daaf985c8670359f5fce9e5cf40042f6285ce8fe
phucnguyen <phucnguyen@moreh.com.vn>
2026-06-10 00:49:36 -0500

    add repobench

--- bench/run_all_full_presets.sh
+++ bench/run_all_full_presets.sh
@@ -20,8 +20,8 @@ ENFORCE_EAGER_PROFILE=0 PRESET=glm5/dp8ep8/CG-zai-org-glm-5-fp8-amd-mi325x-dp8-m
 
 ####### TP8
 ## Enable mtp, bs=64
-ENFORCE_EAGER_PROFILE=0 PRESET=glm5/tp8/TP8-CG-zai-org-glm-5-fp8-amd-mi325x-dp8-moe-tp8-0ic-bs64-dg.yaml    bash run_all.sh
-ENFORCE_EAGER_PROFILE=0 PRESET=glm5/tp8/TP8-CG-zai-org-glm-5-fp8-amd-mi325x-dp8-moe-tp8-0ic-bs64-moreh.yaml    bash run_all.sh
+ENFORCE_EAGER_PROFILE=0 RUN_PROFILE=0 PRESET=glm5/tp8/TP8-CG-zai-org-glm-5-fp8-amd-mi325x-dp8-moe-tp8-0ic-bs64-dg.yaml    bash run_all.sh
+ENFORCE_EAGER_PROFILE=0 RUN_PROFILE=0 PRESET=glm5/tp8/TP8-CG-zai-org-glm-5-fp8-amd-mi325x-dp8-moe-tp8-0ic-bs64-moreh.yaml    bash run_all.sh
 
 ## Disable mtp, bs=64
 # RUN_PROFILE=0 PRESET=glm5/tp8/TP8-zai-org-glm-5-fp8-amd-mi325x-dp8-moe-tp8-0ic-bs64-dg.yaml bash run_all.sh
