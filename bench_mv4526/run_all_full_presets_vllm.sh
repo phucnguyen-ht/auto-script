7da75de425b22b7d4a73282cb525764671e14589
phuc <phuc>
2026-06-24 03:15:16 +0000

    chore

--- bench_mv4526/run_all_full_presets_vllm.sh
+++ bench_mv4526/run_all_full_presets_vllm.sh
@@ -1,3 +1,5 @@
 ################# ENABLE CUDA GRAPH
 # ### DP8EP8
-PRESET=glm5.1/dp8ep8/MTP-bs64-dg.yaml bash run_all.sh
+# PRESET=glm5.1/dp8ep8/MTP-bs64-dg.yaml bash run_all.sh
+# PRESET=glm5.1/dp8ep8/noMTP-bs64-dg.yaml bash run_all.sh
+PRESET=glm5.1/dp8ep8/PDS-MTP-bs64-dg.yaml bash run_all.sh
